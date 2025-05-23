97ee9ad9b963d12aa0bec9844c7de552189ea65f
Elai5 <yvettelaine007@gmail.com>
2025-05-23 15:17:13 +0300

    updated images on asset.js

---
+++ src/assets/assets.js
@@ -0,0 +1,60 @@
+/** @format */
+import desktop_curiosity from "./desktop/image-curiosity.jpg";
+import desktop_deep_earth from "./desktop/image-deep-earth.jpg";
+import desktop_fisheye from "./desktop/image-fisheye.jpg";
+import desktop_from_above from "./desktop/image-from-above.jpg";
+import desktop_grid from "./desktop/image-grid.jpg";
+import desktop_hero from "./desktop/image-hero.jpg";
+import desktop_interactive from "./desktop/image-interactive.jpg";
+import desktop_night_arcade from "./desktop/image-night-arcade.jpg";
+import desktop_pocket_borealis from "./desktop/image-pocket-borealis.jpg";
+import desktop_soccer_team from "./desktop/image-soccer-team.jpg";
+
+import mobile_curiosity from "./mobile/image-curiosity.jpg";
+import mobile_deep_earth from "./mobile/image-deep-earth.jpg";
+import mobile_fisheye from "./mobile/image-fisheye.jpg";
+import mobile_from_above from "./mobile/image-from-above.jpg";
+import mobile_grid from "./mobile/image-grid.jpg";
+import mobile_hero from "./mobile/image-hero.jpg";
+import mobile_interactive from "./mobile/image-interactive.jpg";
+import mobile_night_arcade from "./mobile/image-night-arcade.jpg";
+import mobile_pocket_borealis from "./mobile/image-pocket-borealis.jpg";
+import mobile_soccer_team from "./mobile/image-soccer-team.jpg";
+
+import logo from "./logo.svg";
+import twitter from "./twitter.svg";
+import pinterest from "./pinterest.svg";
+import instagram from "./instagram.svg";
+import hamburger from "./icon-hamburger.svg";
+import facebook from "./facebook.svg";
+import icon_close from "./icon-close.svg";
+
+export const assets = {
+  desktop_curiosity,
+  desktop_deep_earth,
+  desktop_fisheye,
+  desktop_from_above,
+  desktop_grid,
+  desktop_hero,
+  desktop_interactive,
+  desktop_night_arcade,
+  desktop_pocket_borealis,
+  desktop_soccer_team,
+  logo,
+  twitter,
+  pinterest,
+  instagram,
+  hamburger,
+  facebook,
+  icon_close,
+  mobile_curiosity,
+  mobile_deep_earth,
+  mobile_fisheye,
+  mobile_from_above,
+  mobile_grid,
+  mobile_hero,
+  mobile_interactive,
+  mobile_night_arcade,
+  mobile_pocket_borealis,
+  mobile_soccer_team,
+};
